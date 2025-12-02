f18281ce78f010eec5c84f9ae0e884fe4436af55
Stianak-master <stianak@uia.no>
2025-12-02 15:52:52 +0100

    Lagt til config mappen i gazebo pakken

--- src/uiabot_mini_gazebo/setup.py
+++ src/uiabot_mini_gazebo/setup.py
@@ -12,10 +12,16 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
-        (os.path.join('share', package_name, 'worlds'), glob('worlds/*.sdf')),
-        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
     ],
+    # Add optional folders if they exist
+    if os.path.exists('launch'):
+        data_files.append((os.path.join('share', package_name, 'launch'), glob('launch/*.py')))
+    if os.path.exists('config'):
+        data_files.append((os.path.join('share', package_name, 'config'), glob('config/*')))
+    if os.path.exists('worlds'):
+        data_files.append((os.path.join('share', package_name, 'worlds'), glob('worlds/*.sdf')))
+    if os.path.exists('urdf'):
+        data_files.append((os.path.join('share', package_name, 'urdf'), glob('urdf/*'))),
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Group-x',
