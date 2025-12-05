0a91e885c4256dfa2a8f9f3dddeddff66c912071
Stianak-master <stianak@uia.no>
2025-12-05 15:34:44 +0100

    Fixed typos

--- src/uiabot_mini_gazebo/setup.py
+++ src/uiabot_mini_gazebo/setup.py
@@ -4,24 +4,27 @@ from glob import glob
 
 package_name = 'uiabot_mini_gazebo'
 
+data_files = [
+    ('share/ament_index/resource_index/packages',
+        ['resource/' + package_name]),
+    ('share/' + package_name, ['package.xml']),
+]
+
+# Add optional folders if they exist
+if os.path.exists('launch'):
+    data_files.append((os.path.join('share', package_name, 'launch'), glob('launch/*.py')))
+if os.path.exists('config'):
+    data_files.append((os.path.join('share', package_name, 'config'), glob('config/*')))
+if os.path.exists('worlds'):
+    data_files.append((os.path.join('share', package_name, 'worlds'), glob('worlds/*.sdf')))
+if os.path.exists('urdf'):
+    data_files.append((os.path.join('share', package_name, 'urdf'), glob('urdf/*')))
+
 setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
-    # Add optional folders if they exist
-    if os.path.exists('launch'):
-        data_files.append((os.path.join('share', package_name, 'launch'), glob('launch/*.py')))
-    if os.path.exists('config'):
-        data_files.append((os.path.join('share', package_name, 'config'), glob('config/*')))
-    if os.path.exists('worlds'):
-        data_files.append((os.path.join('share', package_name, 'worlds'), glob('worlds/*.sdf')))
-    if os.path.exists('urdf'):
-        data_files.append((os.path.join('share', package_name, 'urdf'), glob('urdf/*'))),
+    data_files=data_files,
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Group-x',
